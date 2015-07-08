f5017d3b1b44d432db72821a11ed51e5dd97d454
HotchLin <lavercat@gmail.com>
2015-07-08 15:41:23 +0800

    新增分頁

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,21 +1,10 @@
 ﻿(function () {
-	angular.module('myApp', [])
+	angular.module('myApp', ['ui.bootstrap'])
 	.controller('patientCtrl', ['$http', '$scope',
 		function ($http, $scope) {
-			$http.get('https://tonyq.org/kptaipei/api-20150628.php')
-				 .success(function (response) {
-				 	$scope.patients = [];
-				 	$scope.patients = response.data;
-
-				 	console.log($scope.patients);
-				 })
-				 .error(function (err) {
-				 	//handle error
-				 })
 
 			//救護狀態css
 			$scope.statusClass = function (status) {
-
 				switch (status) {
 					case '重傷':
 						return 'label-danger';
@@ -28,10 +17,39 @@
 						break;
 					default:
 						break;
-
 				}
-
 			};
+
+			//分頁
+			$scope.filteredPatients = [];
+			$scope.itemsPerPage = 20;
+			$scope.currentPage = 1;
+
+			$scope.patients = [];
+
+			$scope.getPatients = function () {
+				$http.get('https://tonyq.org/kptaipei/api-20150628.php')
+					    .success(function (response) {
+					    	$scope.patients = response.data;
+					    	$scope.paging();
+					    })
+					.error(function (err) {//handle error
+					})
+			};
+
+				$scope.getPatients();
+
+				$scope.paging = function () {
+					var begin = (($scope.currentPage - 1) * $scope.itemsPerPage);
+					var end = begin + $scope.itemsPerPage;
+					$scope.filteredPatients = $scope.patients.slice(begin, end);
+				};
+
+				$scope.pageChanged = function () {
+					$scope.paging();
+				};
+			
+				
 
 		}])
 })();
